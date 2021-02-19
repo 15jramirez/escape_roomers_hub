6cc0003671a84c8dca88ce1b7d2f737c8635d640
jramirez <ramirez.jocelyn73@yahoo.com>
2021-02-19 11:11:22 -0800

    creating login session for user, added flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,18 @@ class SessionsController < ApplicationController
 
     def welcome 
     end
+
+    def new 
+    end
+
+    def create 
+         user = User.find_by(params[:username])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id 
+                redirect_to ()
+            else
+                flash.now[:alert] = "Account was not found"
+                render :new 
+            end
+    end
 end
---
+++ app/models/session.rb
@@ -0,0 +1,4 @@
+class Session < ApplicationRecord
+    validates :username, presence:true
+    validates :password, presence:true
+end
