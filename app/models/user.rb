4576288db3a314f451f6e71694ea8e454b49a68f
jramirez <ramirez.jocelyn73@yahoo.com>
2021-02-19 00:11:25 -0800

    added personalized routes for user and sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_one :profile
+    has_secure_password
     validates :username, presence:true, uniqueness:true, length:{minimum: 6}
     validates :password, length:{in: 6..20}
-    has_secure_password
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  root to: 'sessions#welcome'
   
 end
