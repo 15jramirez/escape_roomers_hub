3d40f54a11fee71d5ea40242f42b09a69b984223
jramirez <ramirez.jocelyn73@yahoo.com>
2021-02-24 23:13:49 -0800

    pushed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_222933) do
+ActiveRecord::Schema.define(version: 2021_02_25_071151) do
 
   create_table "escape_rooms", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_222933) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "rating"
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
