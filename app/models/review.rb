51df024edd0014a39e3ac4a0dcd931b563cab45c
jramirez <ramirez.jocelyn73@yahoo.com>
2021-02-25 00:03:53 -0800

    adding valdidations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
     belongs_to :profile
     belongs_to :escape_room
+    validates :title, presence:true
+    validates :content, presence:true
+    validates :rating, presence:true
 
        def escape_room_attributes=(attr)
         self.escape_room = EscapeRoom.find_or_create_by(title: attr[:title], location: attr[:location], difficulty: attr[:difficulty], hours: attr[:hours], description: attr[:description])
